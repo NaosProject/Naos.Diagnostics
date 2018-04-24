1be0801049db7ee3f1df5403d1b4198fcd51510d
Suraj Gupta <SurajGupta@users.noreply.github.com>
2018-04-24 13:54:04 -0400

    added ProcessHelpers
    renamed MachineName recipe to Machine

---
+++ Naos.Diagnostics.Domain.Test/ProcessHelpersTest.cs
@@ -0,0 +1,131 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProcessHelpersTest.cs" company="Naos">
+//    Copyright (c) Naos 2017. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Diagnostics.Domain.Test
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Naos.Diagnostics.Domain;
+
+    using Xunit;
+
+    public static class ProcessHelpersTest
+    {
+        [Fact]
+        public static void GetRunningProcess___Should_not_return_null___When_called()
+        {
+            // arrange
+
+            // act
+            var actual = ProcessHelpers.GetRunningProcess();
+
+            // assert
+            actual.Should().NotBeNull();
+        }
+
+        [Fact]
+        public static void Name___Should_throw_ArgumentNullException___When_parameter_process_is_null()
+        {
+            // arrange
+
+            // act
+            var actual = Record.Exception(() => ProcessHelpers.Name(null));
+
+            // assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("process");
+        }
+
+        [Fact]
+        public static void Name___Should_not_return_null_or_white_space___When_called()
+        {
+            // arrange
+
+            // act
+            var actual = ProcessHelpers.GetRunningProcess().Name();
+
+            // assert
+            actual.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public static void FilePath___Should_throw_ArgumentNullException___When_parameter_process_is_null()
+        {
+            // arrange
+
+            // act
+            var actual = Record.Exception(() => ProcessHelpers.FilePath(null));
+
+            // assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("process");
+        }
+
+        [Fact]
+        public static void FilePath___Should_not_return_null_or_white_space___When_called()
+        {
+            // arrange
+
+            // act
+            var actual = ProcessHelpers.GetRunningProcess().FilePath();
+
+            // assert
+            actual.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public static void FileVersion___Should_throw_ArgumentNullException___When_parameter_process_is_null()
+        {
+            // arrange
+
+            // act
+            var actual = Record.Exception(() => ProcessHelpers.FileVersion(null));
+
+            // assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("process");
+        }
+
+        [Fact]
+        public static void FileVersion___Should_not_return_null_or_white_space___When_called()
+        {
+            // arrange
+
+            // act
+            var actual = ProcessHelpers.GetRunningProcess().FileVersion();
+
+            // assert
+            actual.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public static void ProductVersion___Should_throw_ArgumentNullException___When_parameter_process_is_null()
+        {
+            // arrange
+
+            // act
+            var actual = Record.Exception(() => ProcessHelpers.ProductVersion(null));
+
+            // assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("process");
+        }
+
+        [Fact]
+        public static void ProductVersion___Should_not_return_null_or_white_space___When_called()
+        {
+            // arrange
+
+            // act
+            var actual = ProcessHelpers.GetRunningProcess().ProductVersion();
+
+            // assert
+            actual.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
---
+++ Naos.Diagnostics.Domain/ProcessHelpers.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProcessHelpers.cs" company="Naos">
+//    Copyright (c) Naos 2017. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Diagnostics.Domain
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Various helper methods related to a processes.
+    /// </summary>
+#if !NaosDiagnosticsDomain
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    [System.CodeDom.Compiler.GeneratedCode("Naos.Diagnostics", "See package version number")]
+#endif
+    internal static class ProcessHelpers
+    {
+        /// <summary>
+        /// Gets the currently running process.
+        /// </summary>
+        /// <returns>
+        /// The currently running process.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "The caller will be the consumer of this recipe.")]
+        public static Process GetRunningProcess()
+        {
+            var result = Process.GetCurrentProcess();
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the process' name.
+        /// </summary>
+        /// <param name="process">The process</param>
+        /// <returns>
+        /// The process' name.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "The caller will be the consumer of this recipe.")]
+        public static string Name(
+            this Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            var result = process.ProcessName;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the file path of a process.
+        /// </summary>
+        /// <param name="process">The process</param>
+        /// <returns>
+        /// The file path of the a process.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "The caller will be the consumer of this recipe.")]
+        public static string FilePath(
+            this Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            var result = process.MainModule.FileName;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the file version of a process.
+        /// </summary>
+        /// <param name="process">The process</param>
+        /// <returns>
+        /// The file version of a process.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "The caller will be the consumer of this recipe.")]
+        public static string FileVersion(
+            this Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            var result = process.MainModule.FileVersionInfo.FileVersion;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the product version of a process.
+        /// </summary>
+        /// <param name="process">The process</param>
+        /// <returns>
+        /// The product version of a process.
+        /// </returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "The caller will be the consumer of this recipe.")]
+        public static string ProductVersion(
+            this Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            var result = process.MainModule.FileVersionInfo.ProductVersion;
+            return result;
+        }
+    }
+}
